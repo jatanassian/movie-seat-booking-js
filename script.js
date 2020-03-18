5ad2e9cc0937eb9d1fa80c2c20bf1d77bdf83d08
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-18 11:09:06 -0400

    update the text (number of seats selected an price) when selecting seats

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)'); // querySelector select just one, querySelectorAll grabs all of them and puts them in an array-like list
+const seats = document.querySelectorAll('.row .seat:not(.occupied)'); // querySelector select just one, querySelectorAll grabs all of them and puts them in an array-like NodeList
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.nodeValue; // The + sign turns the string into an integer. We could also use parseInt()
+const ticketPrice = +movieSelect.value; // The + sign turns the string into an integer. We could also use parseInt()
+
+// Update total and count
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
 
 container.addEventListener('click', (e) => {
   if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) { // "If what we click on has a class that contains 'seat' and not 'occupied' "
     e.target.classList.toggle('selected');
+
+    updateSelectedCount();
   }
 })
